059b4e3df43eb887a5c3e6ce2f032c6359ca5c60
Jean-Michel TEXIER <jm.texier@gmail.com>
2016-12-12 10:34:55 +0800

    Randomize the updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hackstat
 What if you hack your stats?
+
+A little hack just to update public stats on my profile. I am curious to see how it looks after a while and see who will detect the trick ;)
--- hack.sh
+++ hack.sh
@@ -8,18 +8,25 @@ cd $FOLDER
 
 FILENAME="stats/$MONTH.txt"
 
-#check if file has been already updated today
-UPDATED=`stat -q -n -f "%Sm" -t "%F" $FILENAME`
-
-if  [ "$DAY" = "$UPDATED" ]
+#randomize a bit...
+RND=`echo $RANDOM | cut -c 2`
+if [ "$RND" = "2" ] || [ "$RND" = "5" ] || [ "$RND" = "7" ] 
 then
-  echo "stats already hacked for today"
+  #check if file has been already updated today
+  UPDATED=`stat -q -n -f "%Sm" -t "%F" $FILENAME`
+
+  if  [ "$DAY" = "$UPDATED" ]
+  then
+    echo "stats already hacked for today"
+  else
+    echo "Last update was [$UPDATED] and we are [$DAY], need to hack stats ;)" 
+    touch $FILENAME 
+    echo "stats for $DAY $TIME" >> $FILENAME
+    git add .
+    git commit -m "stats for $DAY"
+    git push
+  fi
 else
-  echo "Last update was [$UPDATED] and we are [$DAY], need to hack stats ;)" 
-  touch $FILENAME 
-  echo "stats for $DAY $TIME" >> $FILENAME
-  git add .
-  git commit -m "stats for $DAY"
-  git push
+  echo "we won't update this time... ($RND)"
 fi
 
