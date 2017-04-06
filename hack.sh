1f1a3ee4bbf8fae263b3e6dbc69367ad0ea9cad4
Jean-Michel TEXIER <jm.texier@gmail.com>
2017-04-06 08:15:36 +0800

    Typo :S

--- hack.sh
+++ hack.sh
@@ -12,7 +12,7 @@ cd $FOLDER
 
 FILENAME="stats/$MONTH.txt"
 
-echo "${CLS}Hachink some github stats?...${BLUE}"
+echo "${CLS}Hackink some github stats?...${BLUE}"
 #randomize a bit...
 RND=`echo $RANDOM | cut -c 2`
 if [ "$RND" = "2" ] || [ "$RND" = "5" ] || [ "$RND" = "7" ] 
